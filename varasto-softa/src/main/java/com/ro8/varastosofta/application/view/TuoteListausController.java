33c253c19ca99eaf42c113dd6103d96921a62b2e
Tuukka Mytty <tuukka.mytty@metropolia.fi>
2019-02-14 07:35:13 +0200

    Päivitetty projektia niin, että kun tuotteen valitsee listasta niin sen kaikki tiedot tulevat esiin mahdollista muokkausta varten. Lisätty uusi TuoteProp luokka joka käyttää javafx-propertieseja testausta varten ja mahdollista jatkokäyttöä varten.

--- varasto-softa/src/main/java/com/ro8/varastosofta/application/view/TuoteListausController.java
+++ varasto-softa/src/main/java/com/ro8/varastosofta/application/view/TuoteListausController.java
@@ -1,14 +1,15 @@
 package com.ro8.varastosofta.application.view;
 
+import com.ro8.varastosofta.application.model.TuoteProp;
 import com.ro8.varastosofta.application.model.Validaattori;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -22,6 +23,39 @@ public class TuoteListausController {
 	@FXML
 	private TextField lkmTextField;
 	
+	//--------------------------------
+	// Tässä blokissa koodia testausta varten voi poistaa jos ei tarvetta tai
+	// muokata loppukäyttöä varten.
+	
+	@FXML
+	private TableView<TuoteProp> tpData;
+	@FXML
+	private TableColumn<TuoteProp, String> tuoteNimi;
+	@FXML 
+	private TableColumn<TuoteProp, Integer> tuoteId;
+	
+	private ObservableList<TuoteProp> tuotteet =  FXCollections.observableArrayList();
+	
+	@FXML
+	private void initialize() {
+		// Yhdistetään sarakkeet niitä vastaaviin luokan tietoihin.
+		this.tuoteNimi.setCellValueFactory(new PropertyValueFactory<>("Nimi"));
+		this.tuoteId.setCellValueFactory(new PropertyValueFactory<>("Id"));
+		
+		// Lisätään pari tuotetta testausta varten.
+		this.tuotteet.add(new TuoteProp( 1, "Hila",10));
+		this.tuotteet.add(new TuoteProp(2, "Vitkutin", 15));
+		
+		// Lisätään tuotteet ObservableListaan
+		this.tpData.setItems(this.tuotteet);
+		
+		// Lisätää kuuntelija ja metodi tuotteiden tietojen näyttämistä varten listaan.
+		this.tpData.getSelectionModel().selectedItemProperty().addListener(
+				(observable, oldValue, newValue) -> naytaTuotteenTiedot(newValue));
+	}
+	
+	//--------------------------------
+	
 	
 	@FXML
 	private void muokkaaNappiaPainettu() {
@@ -30,7 +64,7 @@ public class TuoteListausController {
 				this.nimiTextField.getText().toString(), this.lkmTextField.getText().toString())) {
 			
 			// Tähän tuotteen muokkaustoiminnallisuus
-			System.out.println("Nappia painettu");
+			//System.out.println("Nappia painettu");     <- testausta varten voi poistaa
 			
 		} else {
 			Alert alert = new Alert(AlertType.ERROR);
@@ -43,6 +77,13 @@ public class TuoteListausController {
 			alert.showAndWait();
 		}
 		
+	}
+	
+	private void naytaTuotteenTiedot(TuoteProp tuote) {
+		
+		this.idTextField.setText(tuote.getId() + "");
+		this.nimiTextField.setText(tuote.getNimi() + "");
+		this.lkmTextField.setText(tuote.getLkm() + "");
 	}
 	
 }
