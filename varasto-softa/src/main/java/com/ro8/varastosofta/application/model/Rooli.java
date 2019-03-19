06e1ef92116b0b792aa5a74410652a71f505e8ca
Riina Antikainen <riina.antikainen@metropolia.fi>
2019-03-19 12:41:28 +0200

    Lisätty kayttaja ja rooli tietokantataulu-luokat

---
+++ varasto-softa/src/main/java/com/ro8/varastosofta/application/model/Rooli.java
@@ -0,0 +1,33 @@
+package com.ro8.varastosofta.application.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * Rooli tietokantataulu.
+ * @author Riina Antikainen
+ * @author Tuukka Mytty
+ * @author Janne Valle
+ */
+@Entity
+@Table(name="Rooli")
+public class Rooli {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name ="id")
+	private int id;
+	
+	@Column(name ="Nimi")
+	private String nimi;
+	
+	@OneToMany(mappedBy= "kayttaja")
+	private List<Kayttaja> kayttajat;
+}
