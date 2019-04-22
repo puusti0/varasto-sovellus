7e2140edb24ca7728ddb7c05e424d53eeda272b2
Tuukka Mytty <tuukka.mytty@metropolia.fi>
2019-04-22 08:19:38 +0300

    Lokaali luokka lisätty.

---
+++ varasto-softa/src/main/java/com/ro8/varastosofta/application/Lokaali.java
@@ -0,0 +1,30 @@
+package com.ro8.varastosofta.application;
+
+import java.util.Locale;
+
+/**
+ * Luokka lokaalin(locale) asetuksen säilyttämiseen ja asettamiseen/hakemiseen.
+ * 
+ * @author Riina Antikainen
+ * @author Tuukka Mytty
+ * @author Janne Valle
+ *
+ */
+public class Lokaali {
+	
+	private static Locale locale;
+	
+	public static void setLocale(Locale localeToSet) {
+		
+		locale = localeToSet;
+		
+	}
+	
+	public static Locale getLocale() {
+		
+		return locale;
+		
+	}
+	
+
+}
